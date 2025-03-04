8f9fda7997eb4d1b1e15c61a598559cbd374a104
Tsigemariam <tsigemariamzewdu20@gmail.com>
2025-03-04 09:54:23 +0300

    set up prisma and postgresql

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,7 +1,12 @@
+"use client"
 import React from 'react'
 import Link from 'next/link'
+import { usePathname } from 'next/navigation';
 import { AiFillBug } from "react-icons/ai";
+import classNames from 'classnames';
+
 const NavBar = () => {
+    const currentPath=usePathname()
     const links=[
         {label:"Dashboard", href:"/",},
         {label:"Issues", href:"/issues",}
@@ -12,7 +17,7 @@ const NavBar = () => {
 <AiFillBug/>
     </Link>
     <ul className='flex space-x-6'>
-        {links.map(link=><Link key ={link.href} className="text-zinc-500 hover:text-zinc-800 transition-colors" href={link.href}>{link.label}</Link>)}
+        {links.map(link=><Link key ={link.href} className={`${link.href===currentPath?'text-zinc-900':"text-zinc-500"} hover:text-zinc-800 transition-colors`} href={link.href}>{link.label}</Link>)}
         
         </ul></nav>
   )
