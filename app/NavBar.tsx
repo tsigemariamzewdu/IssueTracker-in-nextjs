72a702a0e24540dc83ac2fcc43a967ea049a32a3
Tsigemariam <tsigemariamzewdu20@gmail.com>
2025-03-12 09:26:06 +0300

    change the layout of the navbar

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -4,7 +4,7 @@ import Link from 'next/link'
 import { usePathname } from 'next/navigation';
 import { AiFillBug } from "react-icons/ai";
 import {useSession} from "next-auth/react"
-import { Box } from '@radix-ui/themes';
+import { Box, Container, Flex } from '@radix-ui/themes';
 
 const NavBar = () => {
     const currentPath=usePathname()
@@ -15,7 +15,10 @@ const NavBar = () => {
         {label:"Issues", href:"/issues",}
     ]
   return (
-    <nav className='flex space-x-6 border-b mb-5 px-5 h-14 items-center'>
+    <nav className=' border-b mb-5 px-5 py-3'>
+      <Container>
+      <Flex justify='between'>
+        <Flex align='center' gap='3'>
         <Link href="/">
 <AiFillBug/>
     </Link>
@@ -24,6 +27,8 @@ const NavBar = () => {
         <li key ={link.href}><Link  className={`${link.href===currentPath?'text-zinc-900':"text-zinc-500"} hover:text-zinc-800 transition-colors`} href={link.href}>{link.label}</Link></li>)}
         
         </ul>
+        </Flex>
+        <Box>
         <Box>
           {status==="authenticated" &&(
             <Link href="/api/auth/signout">Log out</Link>
@@ -36,6 +41,11 @@ const NavBar = () => {
 
 
         </Box>
+        </Box>
+        
+      </Flex>
+      
+      </Container>
         </nav>
   )
 }
